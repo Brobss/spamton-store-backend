e847dd50f300a2891915cf13e4d6d7d594e4c715
PauloK3tchup <passandoalimpo13@gmail.com>
2023-08-16 12:01:20 -0300

    da pra fazer upload da imagem pelo menos por enquanto

--- produto/serializers/produto.py
+++ produto/serializers/produto.py
@@ -7,19 +7,21 @@ from uploader.serializers import ImageSerializer
 
 
 class ProdutoSerializer(ModelSerializer):
-    capa_attachment_key = SlugRelatedField(
-        source="capa",
-        queryset=Image.objects.all(),
-        slug_field="attachment_key",
-        required=False,
-        write_only=True,
-    )
-    capa = ImageSerializer(required=False, read_only=True)
+    #capa_attachment_key = SlugRelatedField(
+    #    source="capa",
+    #    queryset=Image.objects.all(),
+    #    slug_field="attachment_key",
+    #    required=False,
+    #    write_only=True,
+    #)
+    #capa = ImageSerializer(required=False, read_only=True)
+
+    class Meta:
+        model = Produto
+        fields = "__all__"
 
 
 class ProdutoListSerializer(ModelSerializer):
-    capa = ImageSerializer(required=False)
-
     class Meta:
         model = Produto
         fields = "__all__"
--- spamton_store_backend/settings.py
+++ spamton_store_backend/settings.py
@@ -126,12 +126,12 @@ MEDIA_URL = "/media/"
 FILE_UPLOAD_PERMISSIONS = 0o640
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissions",
-    ],
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    #"DEFAULT_PERMISSION_CLASSES": [
+    #    "rest_framework.permissions.DjangoModelPermissions",
+    #],
+    #"DEFAULT_AUTHENTICATION_CLASSES": (
+    #    "rest_framework_simplejwt.authentication.JWTAuthentication",
+    #),
 }
 
 AUTH_USER_MODEL = "usuario.Usuario"
