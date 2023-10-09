adeabfafa211ae920394d1e31bc8f7304c38c0da
PauloK3tchup <passandoalimpo13@gmail.com>
2023-10-09 10:08:23 -0300

    limite access token

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,6 +135,10 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=99999),
 }
 
 AUTH_USER_MODEL = "usuario.Usuario"
