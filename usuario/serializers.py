86b949b6c997e61681c0a0a960a6e11d0b8f6857
PauloK3tchup <passandoalimpo13@gmail.com>
2023-10-24 13:09:46 -0300

    não deu certo, recuar!!!

--- usuario/serializers.py
+++ usuario/serializers.py
@@ -8,12 +8,12 @@ class UsuarioSerializer(ModelSerializer):
         model = Usuario
         fields = "__all__"
 
-    #def create(self, validated_data):
-    #    return Usuario.objects.create_user(**validated_data)
+    def create(self, validated_data):
+        return Usuario.objects.create_user(**validated_data)
 
     
-    def create(self, validated_data):
-        user = Usuario
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+    # def create(self, validated_data):
+    #     user = Usuario
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #     return user
